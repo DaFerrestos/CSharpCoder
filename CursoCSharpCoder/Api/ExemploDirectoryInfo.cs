be0332dd8abc8c1175ad44c20c484de17596502f
daferrestos <dabla.ferrestos@gmail.com>
2022-03-04 16:09:15 -0300

    gerenciamento de diretórios Api CSharp

---
+++ CursoCSharpCoder/Api/ExemploDirectoryInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+namespace CursoCSharpCoder.Api
+{
+    class ExemploDirectoryInfo
+    {
+        public static void Executar() {
+            var dirProjeto = @"~/source/repos".ParseHome();
+            var dirInfo = new DirectoryInfo(dirProjeto);
+
+            if (!dirInfo.Exists) {
+                dirInfo.Create();
+            }
+            Console.WriteLine();
+        }
+    }
+}
