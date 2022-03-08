ad704a7ae986ae92b6c04ee8ff9dd357576465f0
daferrestos <dabla.ferrestos@gmail.com>
2022-03-07 21:23:16 -0300

    explorando TimeSpan (espaço de tempo/intervalo de tempo)

---
+++ CursoCSharpCoder/Api/ExemploTimeSpan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharpCoder.Api
+{
+    class ExemploTimeSpan
+    {
+        public static void Executar() {
+            var intervalo = new TimeSpan(days: 10, hours: 20, minutes: 30, seconds: 45);
+            Console.WriteLine(intervalo);
+
+            Console.WriteLine("Intervalo em minutos: " + intervalo.TotalMinutes);
+
+            // Mensurando intervalo de tempo:
+
+            var largada = DateTime.Now;
+            var chegada = DateTime.Now.AddMinutes(15);
+
+            var tempoDeCorrida = chegada - largada;
+
+            Console.WriteLine("Duração da corrida: " + tempoDeCorrida);
+        
+        }
+    }
+}
