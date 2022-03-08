d5c8521f86f20018ccb5f8170bf10ee587de2eac
daferrestos <dabla.ferrestos@gmail.com>
2022-03-07 21:16:29 -0300

    explorando DateTime e Formatos

---
+++ CursoCSharpCoder/Api/ExemploDateTime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharpCoder.Api
+{
+    class ExemploDateTime
+    {
+        public static void Executar() {
+            var dateTime = new DateTime(year: 2035, month: 3, day: 24);
+
+            Console.WriteLine(dateTime.Day);
+            Console.WriteLine(dateTime.Month);
+            Console.WriteLine(dateTime.Year);
+
+            // Sem horas
+
+            var hoje = DateTime.Today;
+            Console.WriteLine(hoje);
+
+            // Com horas
+
+            var agora = DateTime.Now;
+            Console.WriteLine(agora);
+
+            // Manipulação de datas
+
+            var amanha = hoje.AddDays(1);
+            Console.WriteLine(amanha);
+
+            var ontem = hoje.AddDays(-1);
+            Console.WriteLine(ontem);
+
+            // Formatações
+
+            //detalhes de dia
+            Console.WriteLine(hoje.ToString("dd"));
+            Console.WriteLine(hoje.ToString("d"));
+            Console.WriteLine(hoje.ToString("D"));
+
+            //formato descritivo
+            Console.WriteLine(hoje.ToString("dd-MM-yyyy  HH:mm"));
+        }
+    }
+}
--- CursoCSharpCoder/Program.cs
+++ CursoCSharpCoder/Program.cs
@@ -95,6 +95,7 @@ namespace CursoCSharp {
                 {"Diretórios - Usando API", Diretorios.Executar},
                 {"Exemplo DirectoryInfo - Usando API", ExemploDirectoryInfo.Executar},
                 {"Exemplo Path - Usando API", ExemploPath.Executar},
+                {"Exemplo DateTime - Usando API", ExemploDateTime.Executar},
 
 
             });
